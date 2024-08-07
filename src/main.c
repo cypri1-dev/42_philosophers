303d6e6f760ca396fffdb14a023837c5beb1163f
Cyprien Ferreira <cyferrei@bess-f2r6s3.clusters.42paris.fr>
2024-08-07 16:03:04 +0200

    [MODIF]: minor modif

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:43:00 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/08/07 15:10:38 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/08/07 15:59:45 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ int	main(int argc, char **argv, char **envp)
 {
 	(void)argv;
 	parse_philo(argc, argv, envp);
+	printf("Let s go then...!\n");
 	return(0);
 }
