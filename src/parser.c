a5c4617d5d27bb3c5137622211cea1e64d726291
Cyprien Ferreira <cyferrei@bess-f2r6s3.clusters.42paris.fr>
2024-08-07 15:29:27 +0200

    [ADD]: parse case no envp - invalid nb of args - invalid args

--- src/parser.c
+++ src/parser.c
@@ -6,18 +6,55 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 14:26:47 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/08/07 14:45:13 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/08/07 15:24:59 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-void	parse_philo(int argc, char **envp)
+void	check_digit(char **argv)
 {
-	(void)argc;
+	int	i;
+	int	j;
+	
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while(argv[i][j] != '\0')
+		{
+			if(ft_isdigit(argv[i][j]))
+				j++;
+			else
+			{
+				printf("Error!\nOnly digits args are allowed!\n");
+				exit(1);
+			}
+		}
+		i++;
+	}
+}
+
+void	check_nb_args(int argc)
+{
+	if (argc < 5 || argc > 6)
+	{
+		printf("Error!\nInvalid number of arguments.\n");
+		exit(1);
+	}
+}
+
+void	check_envp(char	**envp)
+{
 	if (!*envp)
 	{
 		printf("Error!\nEnvironnement is missing.\n");
-		return;
+		exit(1);
 	}
+}
+void	parse_philo(int argc, char **argv, char **envp)
+{
+	check_envp(envp);
+	check_nb_args(argc);
+	check_digit(argv);
 }
