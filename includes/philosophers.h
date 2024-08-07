445332755f104b3515e2d4f6b695d8402e19e46d
Cyprien Ferreira <cyferrei@bess-f2r6s3.clusters.42paris.fr>
2024-08-07 16:02:43 +0200

    [ADD]: add some prototypes

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:51:41 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/08/07 15:10:19 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/08/07 15:53:45 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void	parse_philo(int argc, char **argv, char **envp);
 void	check_envp(char	**envp);
 void	check_nb_args(int argc);
 void	check_digit(char **argv);
+void	check_overflow(char **argv);
 
 #endif
