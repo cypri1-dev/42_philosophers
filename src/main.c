2cec72b36606bbe61f07121a4a40303db4d49fb1
Cyprien Ferreira <cyferrei@bess-f2r5s14.clusters.42paris.fr>
2024-08-08 14:01:03 +0200

    [MODIF]: minor modif - tests - print_debug

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:43:00 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/08/07 17:23:53 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/08/08 13:59:06 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_data	*data;
 	
+	data = NULL;
 	parse_philo(argc, argv, envp);
-	//init_data();
+	init_data(argc, argv, &data);
+	print_data_struct(data, argc);
+	free(data);
 	return (0);
 }
