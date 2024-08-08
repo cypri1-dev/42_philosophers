3ac12821c6f970778fbc5e8b39d39eaa7d701c76
Cyprien Ferreira <cyferrei@bess-f2r5s14.clusters.42paris.fr>
2024-08-08 15:09:26 +0200

    [ADD/MODIF]: add proper free

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:43:00 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/08/08 13:59:06 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/08/08 15:04:34 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int	main(int argc, char **argv, char **envp)
 	data = NULL;
 	parse_philo(argc, argv, envp);
 	init_data(argc, argv, &data);
-	print_data_struct(data, argc);
+	// print_data_struct(data, argc);
+	// print_philos(data);
+	free(data->philo);
 	free(data);
 	return (0);
 }
