9f9b43a955d0e7fbb8ebb626f569b32e157cca3b
Cyprien Ferreira <cyferrei@bess-f2r5s14.clusters.42paris.fr>
2024-08-08 15:08:58 +0200

    [ADD/MODIF]: move print_debug functin in a new file - program now init data & philos

--- src/init.c
+++ src/init.c
@@ -6,43 +6,48 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 17:25:29 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/08/08 13:59:58 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/08/08 15:03:23 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-void	print_data_struct(t_data *data, int argc)
+void	init_philo(t_data *data)
 {
-	printf("nb_philo: [%d]\n", data->nb_philo);
-	printf("----------------\n");
-	printf("nb_forks: [%d]\n", data->nb_forks);
-	printf("----------------\n");
-	printf("tm_die: [%d ms]\n", data->tm_die);
-	printf("----------------\n");
-	printf("tm_eat: [%d ms]\n", data->tm_eat);
-	printf("----------------\n");
-	printf("tm_sleep: [%d ms]\n", data->tm_sleep);
-	if (argc == 6)
+	int	i;
+	
+	i = 0;
+	while (i < data->nb_philo)
 	{
-		printf("----------------\n");
-		printf("nb_lunch: [%d]\n", data->nb_lunch);
+		data->philo[i].data = data;
+		data->philo[i].r_fork = 1;
+		data->philo[i].l_fork = 0;
+		i++;
 	}
 }
 
 void	init_data(int argc, char **argv, t_data **data)
 {
 	*data = malloc(sizeof(t_data));
-	if (!data)
+	if (!*data)
 	{
 		printf("Error!\nMemory allocation failed for data.\n");
 		exit(EXIT_FAILURE);
 	}
 	(*data)->nb_philo = ft_atoi(argv[1]);
-	(*data)->nb_forks = (*data)->nb_philo;
 	(*data)->tm_die = ft_atoi(argv[2]);
 	(*data)->tm_eat = ft_atoi(argv[3]);
 	(*data)->tm_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
 		(*data)->nb_lunch = ft_atoi(argv[5]);
+	else
+		(*data)->nb_lunch = -1;
+	(*data)->philo = malloc(sizeof(t_philo) * (*data)->nb_philo);
+	if (!(*data)->philo)
+	{
+		printf("Error!\nMemory allocation failed for philo.\n");
+		free(*data);
+		exit(EXIT_FAILURE);
+	}
+	init_philo(*data);
 }
