f7ee0016dd9c9f496c769b586cf742e1222416c2
Cyprien Ferreira <cyferrei@made-f0Ar4s2.clusters.42paris.fr>
2024-08-12 15:51:22 +0200

    [ADD/MODIF]: add end of init with philo

--- src/init.c
+++ src/init.c
@@ -6,11 +6,28 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 17:25:29 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/08/12 14:31:47 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/08/12 15:45:18 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
+
+void	init_philo(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while(i < data->nb_philo)
+	{
+		data->philo[i].id = i;
+		data->philo[i].nb_lunch_philo = 0;
+		data->philo[i].lft_f_id = data->philo[i].id;
+		data->philo[i].rgt_f_id = (data->philo[i].id) + 1 % data->nb_philo;
+		data->philo[i].lst_lunch = 0;
+		data->philo[i].data = data;		
+		i++;
+	}
+}
 
 void	init_mutex(t_data *data)
 {
@@ -20,7 +37,7 @@ void	init_mutex(t_data *data)
 	while(i < data->nb_philo)
 	{
 		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
-			hdl_err_threads(data, "Failed to initialize fork_mutex");
+			hdl_err_philo(data, "Failed to initialize fork_mutex");
 		i++;
 	}
 	if (pthread_mutex_init(&data->checker_lunch, NULL) !=0)
@@ -50,19 +67,21 @@ void	init_data(int argc, char **argv, t_data **data)
 		(*data)->nb_lunch = -1;
 	(*data)->all_lunch = 0;
 	(*data)->dead = 0;
-	// (*data)->philo = malloc(sizeof(t_philo) * (*data)->nb_philo);
-	// if (!(*data)->philo)
-	// {
-	// 	printf("Error!\nMemory allocation failed for philo.\n");
-	// 	free(*data);
-	// 	exit(EXIT_FAILURE);
-	// }
+	(*data)->philo = malloc(sizeof(t_philo) * (*data)->nb_philo);
+	if (!(*data)->philo)
+	{
+		printf("Error!\nMemory allocation failed for philo.\n");
+		free(*data);
+		exit(EXIT_FAILURE);
+	}
 	(*data)->forks = malloc(sizeof(pthread_mutex_t) * (*data)->nb_philo);
 	if (!(*data)->forks)
 	{
 		printf("Error!\nMemory allocation failed for forks.\n");
+		free((*data)->philo);
 		free(*data);
 		exit(EXIT_FAILURE);
 	}
+	init_philo(*data);
 	init_mutex(*data);
 }
