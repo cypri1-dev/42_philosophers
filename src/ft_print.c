e7678e5f1d409692506ca9adf513230987f78c8c
Cyprien Ferreira <cyferrei@made-f0Ar5s5.clusters.42paris.fr>
2024-08-19 14:10:52 +0200

    [MODIF]: minors modif on prints

--- src/ft_print.c
+++ src/ft_print.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 13:56:52 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/08/16 17:30:36 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/08/19 13:28:00 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 void	ft_print_dead_mutex(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->data->print_mutex);
-	printf("%ld Philosopher [%d] died\n", get_curr_time(), philo->id);
+	printf("%ld Philo [%d] died\n", get_curr_time(), philo->id);
 	pthread_mutex_unlock(&philo->data->print_mutex);
 }
 
 void	ft_print_think_mutex(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->data->print_mutex);
-	printf("%ld Philosopher [%d] is thinking\n", get_curr_time(), philo->id);
+	printf("%ld Philo [%d] is thinking\n", get_curr_time(), philo->id);
 	pthread_mutex_unlock(&philo->data->print_mutex);
 }
 
 void	ft_print_sleep_mutex(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->data->print_mutex);
-	printf("%ld Philosopher [%d] is sleeping\n", get_curr_time(), philo->id);
+	printf("%ld Philo [%d] is sleeping\n", get_curr_time(), philo->id);
 	pthread_mutex_unlock(&philo->data->print_mutex);
 }
 
@@ -37,7 +37,7 @@ void	ft_print_eat_mutex(t_philo *philo)
 {
 	// pthread_mutex_lock(&philo->data->checker_lunch);
 	pthread_mutex_lock(&philo->data->print_mutex);
-	printf("%ld Philosopher [%d] is eating\n", get_curr_time(), philo->id);
+	printf("%ld Philo [%d] is eating\n", get_curr_time(), philo->id);
 	setter_time(&philo->data->checker_lunch, &philo->lst_lunch, get_curr_time());
 	// philo->lst_lunch = get_curr_time();
 	pthread_mutex_unlock(&philo->data->print_mutex);
@@ -47,6 +47,6 @@ void	ft_print_eat_mutex(t_philo *philo)
 void	ft_print_forks_mutex(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->data->print_mutex);
-	printf("%ld Philosopher [%d] took a fork\n", get_curr_time(), philo->id);
+	printf("%ld Philo [%d] took a fork\n", get_curr_time(), philo->id);
 	pthread_mutex_unlock(&philo->data->print_mutex);
 }
